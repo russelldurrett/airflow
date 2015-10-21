3bfd1cdf31cb42ed40289e626245883842d6c462
Russell Durrett <russdurrett@gmail.com>
2015-10-21 14:27:01 -0500

    comment out future and urllib imports

--- airflow/configuration.py
+++ airflow/configuration.py
@@ -1,5 +1,5 @@
-from future import standard_library
-standard_library.install_aliases()
+# from future import standard_library
+# standard_library.install_aliases()
 from builtins import str
 from configparser import ConfigParser
 import errno
