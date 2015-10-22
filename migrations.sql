a2a4ae59b66e0b126fd7a9fbf3437106c9ff6bde
Russell Durrett <russdurrett@gmail.com>
2015-10-21 19:12:02 -0500

    rm random migration file

--- migrations.sql
+++
@@ -1,5 +0,0 @@
-// To 0.7
-alter table task_instance add column queue varchar(50) NULL;
-alter table task_instance add column pool varchar(50) NULL;
-alter table task_instance add column priority_weight INT NULL;
-create index ti_pool on task_instance (pool, state) using btree;
